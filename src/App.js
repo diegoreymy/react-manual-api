f6dc5675ab9d8b6ccf4ff65cd3e410dd7071c390
diego.h.aguilar <diego.h.aguilar@accenture.com>
2019-02-15 18:00:47 -0300

    Manual de uso de la API

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <header className="main-header">
+        <section className="wrap">
+          <h1 className="logo"><a href="/">https://diegoreymy.herokuapp.com/</a></h1>
+        </section>
+		  </header>
         <Endpoints />
       </div>
     );
